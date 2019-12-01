579a272ac44e4afbb61ecc1e5aa547cb222ffe66
sgildart <sgildart@jaguarlandrover.com>
2019-12-01 20:49:13 +0000

    AoC Day 1, refactored part 1 for part 2 solution

---
+++ day-1-fuel-requirements/totalFuelCalculator.js
@@ -0,0 +1,28 @@
+const { inputArray } = require("./input");
+
+
+const fuelCalculator = n => Math.floor(n / 3) - 2;
+
+const fuelOffsetCalculator = value => {
+  const LOWEST_OFFEST_VALUE = 9;
+  const totalFuel = fuelCalculator(value);
+  let fuelOffset = [totalFuel];
+  let latestFuelOffset = totalFuel;
+  
+  while (latestFuelOffset >= LOWEST_OFFEST_VALUE) {
+    const result = fuelCalculator(latestFuelOffset);
+    fuelOffset.push(result);
+    latestFuelOffset = result;
+  }
+  
+  return fuelOffset.reduce((sum, value) => sum += value, 0);
+
+}
+
+const totalFuelCalculator = inputArray =>
+  inputArray.reduce((sum, value) => (sum += fuelOffsetCalculator(value)), 0);
+
+module.exports = { fuelCalculator, totalFuelCalculator, fuelOffsetCalculator };
+
+
+console.log(totalFuelCalculator(inputArray));
---
+++ day-1-fuel-requirements/totalFuelCalculator.spec.js
@@ -0,0 +1,41 @@
+const { fuelCalculator, totalFuelCalculator, fuelOffsetCalculator } = require("./totalFuelCalculator");
+const { expect } = require("chai");
+
+describe("fuelCalculator", () => {
+  it("for mass of 12, returns 2", () => {
+    expect(fuelCalculator(12)).to.equal(2);
+  });
+  it("for mass of 14, returns 2", () => {
+    expect(fuelCalculator(14)).to.equal(2);
+  });
+  it("for mass of 1969, returns 654", () => {
+    expect(fuelCalculator(1969)).to.equal(654);
+  });
+  it("for mass of 100756, returns 33583", () => {
+    expect(fuelCalculator(100756)).to.equal(33583);
+  });
+});
+
+describe("fuelOffsetCalculator", () => {
+  it("for mass of 14, returns 2", () => {
+    expect(fuelOffsetCalculator(14)).to.equal(2);
+  });
+  it("for mass of 1969, returns 654", () => {
+    expect(fuelOffsetCalculator(1969)).to.equal(966);
+  });
+  it("for mass of 100756, returns 33583", () => {
+    expect(fuelOffsetCalculator(100756)).to.equal(50346);
+  });
+});
+
+describe("totalFuelCalculator", () => {
+  it.only("for mass of 10, returns 1", () => {
+    expect(totalFuelCalculator([10])).to.equal(1);
+  });
+  it("for masses of 12 and 14, returns 4", () => {
+    expect(totalFuelCalculator([12, 14])).to.equal(4);
+  });
+  it("for masses of 12, 14 and 1969, returns 658", () => {
+    expect(totalFuelCalculator([12, 14, 1969])).to.equal(970);
+  });
+});
